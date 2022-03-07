3539ee2f8621bc31ec04970c1b4aa46e0553d3f3
XavierTwitty <x.twitty@gmail.com>
2022-03-07 17:09:57 -0500

    done

--- api/server.js
+++ api/server.js
@@ -1,3 +1,93 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const Users = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+
+
+server.get('/api/users/', (req, res)=> {
+    Users.find()
+        .then (users => {
+            res.json(users)
+        })
+})
+server.get('/api/users/:id', (req, res)=> {
+    Users.findById(req.params.id)
+        .then(user => {
+            if(user == null) {
+                res.status(404).json({message: `dog with id ${req.params.id} not found!`})
+            } else {
+                res.status(200).json(user)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message });
+            
+        })
+})
+server.post('/api/users/', (req, res)=> {
+    const body = req.body;
+
+    if(!req.body.name) {
+        res.status(400).json({ message: 'missing name in request' });
+        return;
+    } else if(!req.body.bio) {
+        res.status(400).json({ message: 'missing weight in request' });
+        return;
+    }
+
+
+    Users.insert(body)
+    .then(user => {
+        res.status(201).json(user);
+    })
+    .catch(err => {
+        res.status(500).json({ message: err.message });
+    });
+
+})
+server.put('/api/users/:id', (req, res)=> {
+    const user = req.body
+
+    if(!req.body.name) {
+        res.status(400).json({ message: 'missing name in request' });
+        return;
+    } else if(!req.body.bio) {
+        res.status(400).json({ message: 'missing weight in request' });
+        return;
+    }
+
+    Users.update(req.params.id, user)
+        .then(user => {
+            if(user == null) {
+                res.status(404).json({ message: `user with id ${req.params.id} not found!`});
+            } else {
+                res.status(200).json(user);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message });
+        });
+
+})
+server.delete('/api/users/:id', (req, res)=> {
+    Users.remove(req.params.id)
+    .then(user => {
+        if(user == null) {
+            res.status(404).json({ message: `user with id ${req.params.id} not found!`});
+        } else {
+            res.status(200).json(user);
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: err.message });
+    });
+})
+
+
+
+
+
+module.exports = server; // EXPORT YOUR SERVERreq, res of {}
